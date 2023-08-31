7ac319ed6ba8a4f5aff065ed051c4f3a3e3b58b4
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-08-31 14:36:06 -0500

    updated keys, inserted mock data, built database

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,5 +1,11 @@
 const client = require('./client')
 
+const { createUser, getAllUsers } = require('./helpers/users')
+const { createPost, getPostsById } = require('./helpers/posts')
+const { createLikes } = require('./helpers/likes')
+
+const { users, posts, likes } = require('./seedData')
+
 // drop tables
 const dropTables = async () => {
     try {
@@ -22,19 +28,95 @@ const createTables = async () => {
     console.log("building tables")
     await client.query(`
     CREATE TABLE users (
-        
+        user_id SERIAL PRIMARY KEY,
+        username varchar(30) UNIQUE NOT NULL,
+        password varchar(30) NOT NULL,
+        first_name varchar NOT NULL,
+        last_name varchar,
+        email varchar NOT NULL,
+    );
+    CREATE TABLE posts (
+        post_id SERIAL PRIMARY KEY,
+        book_image image,
+        book_title varchar,
+        book_author varchar,
+        book_summary varchar,
+        user_id INTEGER REFERENCES user(user_id)
+    );
+    CREATE TABLE  likes (
+        likes_id SERIAL PRIMARY KEY,
+        user_id INTEGER REFERENCES user(user_id),
+        post_id INTEGER REFERENCES post(post_id),
     )
     `)
+    console.log("tables are built")
 }
 
-// insert mock data from seedData.js
-
+// INSERT MOCK DATA
+// 
 // create users
-
+const createInitialUsers = async () => {
+    try {
+        // looping through "users" array from seedData
+        for (const user of users) {
+            // insert each user into the table
+            await createInitialUser(user)
+        }
+        console.log("created users")
+    } catch (error) {
+        throw error
+    }
+}
 
 // create posts
+const createInitialPosts = async () => {
+    try {
+        // looping through "posts" array from seedData
+        for (const post of posts) {
+            // insert each post into the table
+            await createInitialPost(post)
+        }
+        console.log("create posts")
+    } catch (error) {
+        throw error
+    }
+}
 
 // create likes
-
+const createInitialLikes = async () => {
+    try {
+        // looping through "likes" array from seedData
+        for (const like of likes) {
+            // insert each post into the table
+            await createInitialLikes(like)
+        }
+        console.log("created likes")
+    } catch (error) {
+        throw error
+    }
+}
 
 // call all my functions and 'build' my database
+const rebuildDb = async () => {
+    try {
+        // connect to my local database
+        client.connect()
+        // run our functions
+        await dropTables()
+        await createTables()
+
+        // generating starting data
+        console.log("starting to seed")
+        await createInitialUsers()
+        await createInitialPosts()
+        await createInitialLikes()
+
+    } catch (error) {
+        console.log(error)
+    } finally{
+        // close our connection
+        client.end()
+    }
+}
+
+rebuildDb()
