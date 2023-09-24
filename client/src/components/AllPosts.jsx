cf0cde7224b1ae94b7f69dadc65033b8b36c0723
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-09-24 15:28:38 -0500

    Fixed import issue

--- client/src/components/AllPosts.jsx
+++ client/src/components/AllPosts.jsx
@@ -1,7 +1,7 @@
 // DISPLAYS NAV BAR & ALL OF THE POSTS
 import { useState, useEffect } from "react";
 import { fetchAllPosts, fetchUsers } from "../API";
-import Card from "@mui/materia/Card";
+import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 
