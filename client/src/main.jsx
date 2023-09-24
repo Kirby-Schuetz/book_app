3460018ef41ac28ac0b8e1984e732c432e967117
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-09-24 11:39:16 -0500

    Copy App to new file and delete everything to debug Component rendering issue.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,21 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 // import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { configureStore } from '@reduxjs/toolkit';
-// import userReducer from "./components/Authentication.js";
-
-// const store = configureStore({
-//   reducer: {
-//           user: userReducer,
-//   },
-// });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
     <App />
     {/* <BrowserRouter></BrowserRouter> */}
-    {/* </Provider> */}
   </React.StrictMode>,
 )
