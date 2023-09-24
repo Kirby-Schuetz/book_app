d93d33bad5754a7685ca888e7bae709aa5e1fdf0
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-09-24 10:11:38 -0500

    Continue debugging

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import AllPosts from "./components/AllPosts";
+// import { Routes, Route } from "react-router-dom";
+// import AllPosts from "./components/AllPosts";
 // import UserProfile from "./components/UserProfile";
 // import CreatePostForm from "./components/CreatePostForm";
-// import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar";
 // import LoginPage from "./components/LoginPage";
 // import CreateUserForm from "./components/CreateUserForm";
 // import EditPostForm from "./components/EditPostForm";
@@ -18,20 +18,20 @@ function App() {
   return (
     <>
       <div>
-        {/* <div id="navbar"><NavBar /></div> */}
+        <div id="navbar"><NavBar /></div>
         <div className="main-section">
         <h1>Welcome to Bored Bibliophile</h1>
-        <LoginContextProvider>
+        {/* <LoginContextProvider>
           <Routes>
-            {/* <Route path="/LoginPage" element={<LoginPage />} /> */}
+            <Route path="/LoginPage" element={<LoginPage />} />
             <Route path="/" element={<AllPosts />} />
-            {/* <Route path="/CreatePostForm" element={<CreatePostForm />} />
+            <Route path="/CreatePostForm" element={<CreatePostForm />} />
             <Route path="/UserProfile" element={<UserProfile />} />
             <Route path="/CreateUserForm" element={<CreateUserForm />}/>
             <Route path="/EditPostForm" element={<EditPostForm />}/>
-            <Route path="/EditUserForm" element={<EditUserForm />}/> */}
+            <Route path="/EditUserForm" element={<EditUserForm />}/>
           </Routes>
-        </LoginContextProvider>
+        </LoginContextProvider> */}
         </div>
       </div>
     </>
