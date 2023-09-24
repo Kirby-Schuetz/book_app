6cfe3921e34ad87030abc1e634d5282811830da7
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-09-24 09:15:15 -0500

    Debugging deploy issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,24 @@
-import { Routes, Route } from "react-router-dom";
-import AllPosts from "./components/AllPosts";
-import UserProfile from "./components/UserProfile";
-import CreatePostForm from "./components/CreatePostForm";
-import NavBar from "./components/NavBar";
-import LoginPage from "./components/LoginPage";
-import CreateUserForm from "./components/CreateUserForm";
-import EditPostForm from "./components/EditPostForm";
-import EditUserForm from "./components/EditUserForm";
-import { LoginContextProvider } from "./context/loginContext";
-import { useState } from 'react';
+// import { Routes, Route } from "react-router-dom";
+// import AllPosts from "./components/AllPosts";
+// import UserProfile from "./components/UserProfile";
+// import CreatePostForm from "./components/CreatePostForm";
+// import NavBar from "./components/NavBar";
+// import LoginPage from "./components/LoginPage";
+// import CreateUserForm from "./components/CreateUserForm";
+// import EditPostForm from "./components/EditPostForm";
+// import EditUserForm from "./components/EditUserForm";
+// import { LoginContextProvider } from "./context/loginContext";
+// import { useState } from 'react';
 
 import './App.css';
 
 function App() {
-  const [token, setToken] = useState(null);
+  // const [token, setToken] = useState(null);
  
   return (
     <>
       <div>
-        <div id="navbar"><NavBar /></div>
+        {/* <div id="navbar"><NavBar /></div> */}
         <div className="main-section">
         <h1>Welcome to Bored Bibliophile</h1>
         {/* <LoginContextProvider>
