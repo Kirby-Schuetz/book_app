738ec8153e071c8802949c3a3cbfa9da78597903
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-09-24 10:32:14 -0500

    Updating NavBar with MaterialUI appbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,18 +1,49 @@
 // DISPLAYS HOME BUTTON->ALLPOSTS, SEARCH BAR, CREATE POST BUTTON->CREATPOST, USER PROFILE->USERPROFILE
 // import { Link } from "react-router-dom";
-import SearchBar from "./SearchBar"
+// import SearchBar from "./SearchBar"
 // import UserProfile from ".UserProfile";
 
 
-export default function NavBar() {
+// export default function NavBar() {
  
-return (
-        <nav className="navbar">
-            {/* <Link to="/LoginPage">Home</Link> */}
-            <SearchBar/>
-            {/* <Link to="/">Library</Link> */}
-            {/* <Link to="/CreatePostForm">Post</Link>
-            <Link to="/UserProfile">Profile</Link> */}
-        </nav>
-    );
+// return (
+//         <nav className="navbar">
+//             {/* <Link to="/LoginPage">Home</Link> */}
+//             {/* <SearchBar/> */}
+//             {/* <Link to="/">Library</Link> */}
+//             {/* <Link to="/CreatePostForm">Post</Link>
+//             <Link to="/UserProfile">Profile</Link> */}
+//         </nav>
+//     );
+// }
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function NavBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
