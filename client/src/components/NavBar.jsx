9518a428c11a0743f1fd9505db5fe0d16cdf6d94
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-09-24 16:07:00 -0500

    Style navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -28,25 +28,27 @@ import MenuIcon from '@mui/icons-material/Menu';
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+      <nav className="navbar">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
 
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link to="/CreatePostForm">Post</Link>
-          </Typography>
-          <SearchBar />
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to="/CreatePostForm">Post</Link>
+            </Typography>
+            <SearchBar />
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </nav>
     </Box>
   );
 }
