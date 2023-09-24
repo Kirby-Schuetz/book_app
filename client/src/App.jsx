c1fe7b7ed82f36dc645f17c38292851acfe81fac
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-09-24 10:43:14 -0500

    Move navbar components into App for debugging purposes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,21 @@
 // import AllPosts from "./components/AllPosts";
 // import UserProfile from "./components/UserProfile";
 // import CreatePostForm from "./components/CreatePostForm";
-import NavBar from "./components/NavBar";
+// import NavBar from "./components/NavBar";
 // import LoginPage from "./components/LoginPage";
 // import CreateUserForm from "./components/CreateUserForm";
 // import EditPostForm from "./components/EditPostForm";
 // import EditUserForm from "./components/EditUserForm";
 // import { LoginContextProvider } from "./context/loginContext";
 // import { useState } from 'react';
-
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   // const [token, setToken] = useState(null);
@@ -18,7 +24,25 @@ function App() {
   return (
     <>
       <div>
-        <NavBar />
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                News
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
         <div className="main-section">
         {/* <h1>Welcome to Bored Bibliophile</h1> */}
         {/* <LoginContextProvider>
