65aed88120b68aa673502e436b349cc527337d8b
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-09-24 14:59:29 -0500

    Test out new NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Login from "./components/Login";
+import NavBar from "./components/NavBar";
 // import './App.css';
 
 
@@ -7,7 +7,7 @@ function App() {
  
   return (
     <>
-      <Login />
+      <NavBar />
     </>
   );
 }
